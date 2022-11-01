2d3133ae94d323952cf255e6ed048eacbf843c35
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-11-01 14:56:53 -0300

    mapeo según esquemático

--- tdp1/arcade_stick/app/inc/mapeoGpio.h
+++ tdp1/arcade_stick/app/inc/mapeoGpio.h
@@ -22,22 +22,33 @@
 
    // ---------------------------- joystick -------------------------
 
-   #define PIN_VRX     CH3
-   #define PIN_VRY     CH2
-   #define PIN_SW      T_COL1
+   #define PIN_VRX   CH3
+   #define PIN_VRY   CH2
+   #define PIN_SW    T_COL1
 
    // ---------------------------- pulsadores -----------------------
 
-   
-
-
+   #define PIN_S1    GPIO1
+   #define PIN_S2    GPIO3
+   #define PIN_S3    GPIO5
+   #define PIN_S4    GPIO7
+   #define PIN_S5    GPIO8
+   #define PIN_S6    GPIO6
 
    // ---------------------------- display --------------------------
 
-
-
+   #define PIN_LCD_E    LCDEN
+   #define PIN_LCD_RS   LCDRS
+   #define PIN_LCD_D4   LCD1
+   #define PIN_LCD_D5   LCD2
+   #define PIN_LCD_D6   LCD3
+   #define PIN_LCD_D7   LCD4
 
    // ---------------------------- leds -------------------------------
+
+   #define PIN_LED_R    GPIO0
+   #define PIN_LED_G    GPIO2
+   #define PIN_LED_B    GPIO4
 
 
 /*==================[end of file]============================================*/
